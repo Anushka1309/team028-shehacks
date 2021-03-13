1a707efb61c69bc4896db12ebb1b259bbd2ac170
janhaviiii <76651032+janhaviiii@users.noreply.github.com>
2021-03-13 20:41:47 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,113 +1,19 @@
-import 'package:akeeva/constants.dart';
 import 'package:flutter/material.dart';
 
-import 'constants.dart';
-void main() => runApp(MyApp());
+import 'LoginPage.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Akeeva',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: kBackgroundColor,
-        textTheme: TextTheme(
-          headline3: TextStyle(color: Colors.pink[100], fontWeight: FontWeight.w500),
-          button: TextStyle(color: kPrimaryColor),
-          headline5:
-              TextStyle(color: Colors.pink[100], fontWeight: FontWeight.w400),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.white.withOpacity(.2),
-            ),
-          ),
-        ),
-      ),
-      home: WelcomeScreen(),
-    );
-  }
+void main() {
+  runApp(MyApp());
 }
 
-class WelcomeScreen extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/image.jpeg'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "Akeeva!\n",
-                        style: Theme.of(context).textTheme.headline3,
-                      ),
-                      TextSpan(
-                        text: "We are for you!",
-                        style: Theme.of(context).textTheme.headline5,
-                      )
-                    ],
-                  ),
-                ),
-                FittedBox(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) {
-                          return;
-                        },
-                      ));
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 30),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 30, vertical: 16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: kPrimaryColor,
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Text(
-                            "Welcome",
-                            style: Theme.of(context).textTheme.button.copyWith(
-                                  color: Colors.black,
-                                ),
-                          ),
-                          SizedBox(width: 10),
-                          Icon(
-                            Icons.arrow_forward,
-                            color: Colors.black,
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+
     );
   }
 }
